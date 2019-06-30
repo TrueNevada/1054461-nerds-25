9bd7a23c236ddbd96ce8e00097ed9495b7010974
TrueNevada <ricefieldsevilsamurai@gmail.com>
2019-07-01 00:07:58 +0300

    Внесение корректировок

--- js/popup.js
+++ js/popup.js
@@ -1,19 +1,23 @@
 var link = document.querySelector(".write-us-link");
 var popup = document.querySelector(".modal-form");
-var close = document.querySelector(".modal-close");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector("form");
+var text = popup.querySelector("[name=your-name]");
+var text = popup.querySelector("[name=email]");
+var text = popup.querySelector("[name=write-text]");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
+  your-name.focus();
 });
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
 });
-window.addEventListener("keydown", function (evt) {
-  if (evt.keyCode === 27) {
-    if (popup.classList.contains("modal-show")) {
-      evt.preventDefault();
-      popup.classList.remove("modal-show");
-    }
+form.addEventListener("submit", function (evt) {
+  if (!text.value || !text.value || !text.value ) {
+    evt.preventDefault();
+    console.log("Заполните поля");
   }
 });
+
