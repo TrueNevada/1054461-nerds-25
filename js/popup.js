96ab44d6ec4ce80ad3513e9727cc6605eabc0970
TrueNevada <ricefieldsevilsamurai@gmail.com>
2019-07-03 23:34:51 +0300

    Исправление переполнения контента

--- js/popup.js
+++ js/popup.js
@@ -8,23 +8,23 @@ var text = popup.querySelector("[name=write-text]");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
-  your-name.focus();
+  your - name.focus();
 });
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
 });
 form.addEventListener("submit", function (evt) {
-  if (!text.value || !text.value || !text.value ) {
+  if (!text.value || !text.value || !text.value) {
     evt.preventDefault();
     console.log("Заполните поля");
   }
 });
- window.addEventListener("keydown", function (evt) {
- if (evt.keyCode === 27) {
- if (popup.classList.contains("modal-show")) {
- evt.preventDefault();
- popup.classList.remove("modal-show");
- }
- }
- }); 
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      popup.classList.remove("modal-show");
+    }
+  }
+});
