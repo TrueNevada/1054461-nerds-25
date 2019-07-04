f55c22ca73fb0f95ed172cfca5677640c3cc3dee
TrueNevada <ricefieldsevilsamurai@gmail.com>
2019-07-05 00:48:28 +0300

    Внесение корректировок

--- js/popup.js
+++ js/popup.js
@@ -1,25 +1,48 @@
 var link = document.querySelector(".write-us-link");
 var popup = document.querySelector(".modal-form");
-var close = popup.querySelector(".modal-close");
+var close = document.querySelector(".modal-close");
+var fullname = popup.querySelector("[name=name]");
 var form = popup.querySelector("form");
-var text = popup.querySelector("[name=your-name]");
-var text = popup.querySelector("[name=email]");
-var text = popup.querySelector("[name=write-text]");
+var email = popup.querySelector("[name=email]");
+var text = popup.querySelector("[name=text]");
+var storage = localStorage.getItem("email");
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("email");
+} catch (err) {
+  isStorageSupport = false;
+}
+
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
-  your - name.focus();
+  fullname.focus();
+  if (storage) {
+    email.value = storage;
+  }
 });
+
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
 });
+
 form.addEventListener("submit", function (evt) {
-  if (!text.value || !text.value || !text.value) {
+  if (!email.value || !text.value) {
     evt.preventDefault();
-    console.log("Заполните поля");
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem("email", email.value);
+    }
   }
 });
+
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     if (popup.classList.contains("modal-show")) {
